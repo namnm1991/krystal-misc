34246eaedf9f89347641d96f1e994bbe869f71e0
Nam Nguyen <namnm1991@gmail.com>
2021-01-26 18:09:26 +0700

    Signing message with Metamask wallet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import { useState } from 'react';
+
+let ethereum = window.ethereum;
 
 function App() {
+  // if (!ethereum) {
+  //   return (
+  //     <p>Please install Metamask</p>
+  //   )
+  // }
+  let [account, setAccount] = useState();
+  let [signature, setSignature] = useState();
+  let refCode = "a";
+  
+  let web3 = new Web3(ethereum)
+  console.log(web3.eth.accounts)
+
+  async function connect() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+    setAccount(accounts[0])
+    web3.eth.personal.sign(refCode, accounts[0]).then((sig) => {setSignature(sig)})
+  }  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Sign message</h3>
+      <p>Hello account: {account}</p>
+      <p>Code: {refCode}</p>
+      <p>signature: {signature}</p>
+      <button onClick={connect}>Connect Metamask</button>
     </div>
-  );
+  )
 }
 
 export default App;
